4ca3caf1f968f9ef40c7bde3fa0b26bdf11cd0bb
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-10-30 16:07:24 +0700

    add main
    - add main
    - add interaction utils

---
+++ src/utils/interaction/search_local.py
@@ -0,0 +1,5 @@
+from src.abstractions.db_manager_abstract import DBManagerAbstract
+
+
+def search_local(db_manager: DBManagerAbstract):
+    pass
---
+++ src/utils/interaction/utils.py
@@ -0,0 +1,12 @@
+def user_choice(choice_list: list[str]) -> int:
+    """
+    User choice dialog
+    :param choice_list: strings for choice list
+    :return: chosen index or -1 if error
+    """
+    try:
+        choice_list = '\n'.join([f' {i + 1}: {a}' for i, a in enumerate(choice_list)])
+        print(f"Выберите дальнейшее действие:\n{choice_list}\n")
+        return int(input("> "))
+    except ValueError:
+        return -1
